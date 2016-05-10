e2b78b68bec6742ac4869049da293bb0c97bb171
Timo Weiß <info@timo-weiss.com>
2016-05-10 22:13:45 +0200

    init plugins

---
+++ api/plugins/user/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+exports.register = (server, options, next) => {
+    
+    server.route({
+        method: 'GET',
+        path: '/users',
+        handler: function (request, reply) {
+            reply('user index');
+        }
+    });
+
+    next();
+};
+
+exports.register.attributes = {
+    name: 'user',
+    version: '1.0.0'
+};

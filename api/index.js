7792a4667c4e0914811be0d53b3c66e6214835f4
Timo Weiß <info@timo-weiss.com>
2016-05-10 22:02:00 +0200

    init setup

---
+++ api/index.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const Glue = require('glue');
+const hoek = require('hoek');
+
+const unwrap = require('./lib/responseCodes').unwrap;
+
+const API_PORT = process.env['API_PORT'] || 2020;
+
+const manifest = {
+    connections: [{
+        port: API_PORT,
+        // TODO remove in production
+        routes: {
+            cors: true
+        }
+    }],
+    registrations: [{
+        plugin: 'inert'
+    }, {
+        plugin: 'vision'
+    }, {
+        plugin: 'hapi-swagger'
+    }, {
+        plugin: 'hapi-auth-cookie'
+    }]
+};
+
+Glue.compose(manifest, {relativeTo: __dirname})
+    .then(server => {
+
+        server.on('route', (route, connection, server) => {
+
+            console.log('New route added: ' + route.path);
+        });
+
+        server.on('log', (event, tags) => {
+            console.log(event, tags);
+        });
+
+        // configure auth strategy
+        server.auth.strategy('session', 'cookie', true, {
+            password: process.env['COOKIE_SECRET'] || 'secretzweiunddreisigzeichenmindestens',
+            ttl: 24 * 60 * 60 * 1000 * 365,   // 1 year
+            keepAlive: true,
+            cookie: 'invoicesession',
+            isSecure: false, //TODO
+            clearInvalid: true,
+            isHttpOnly: false // TODO
+        });
+
+
+        server.ext('onPostAuth', (request, reply) => {
+            let requestAuth = request.auth;
+            request.requesting_user_id = {};
+            request.company_id = requestAuth.credentials ? requestAuth.credentials.company_id : '';
+            request.requesting_user_id.ruid = requestAuth.credentials && requestAuth.credentials.user._id ? requestAuth.credentials.user._id : 'unknown';
+
+
+            // delay all requests to simulate network-latency for the frontend guys ;)
+            let delay = process.env['PRODUCTION'] ? 0 : 1000;
+
+            // setTimeout(function () {
+            reply.continue();
+            // }, delay);
+
+        });
+
+        server.decorate('request', 'applyToDefaults', hoek.applyToDefaults);
+
+        server.decorate('request', 'unwrap', unwrap);
+
+
+        return server;
+    })
+    .then(server => server.start())
+    .then(() => console.log(`Server running at port ${API_PORT}`))
+    .catch(console.error);
---
+++ api/lib/responseCodes.js
@@ -0,0 +1,43 @@
+'use strict';
+
+'use strict';
+
+const boom = require('boom');
+
+module.exports = {
+    unwrap
+};
+
+const ERRORS = {
+    NOT_FOUND: {fn: boom.notFound},
+    LOGIN_NOT_FOUND: {fn: boom.unauthorized, details: 'Wrong mail or password'},
+    WRONG_PASSWORD: {fn: boom.unauthorized, details: 'Wrong mail or password'},
+    BAD_IMPL: {fn: boom.badImplementation},
+    MISSING_COMPANY_ID_SESSION: {fn: boom.badRequest, details: 'Please define a company_id for your session'},
+    USER_ALREADY_EXISTS: {fn: boom.badRequest, details: 'User already exists'}
+};
+
+
+function unwrap(serviceResponse) {
+    if (serviceResponse && !serviceResponse.err && serviceResponse.data) {
+        return serviceResponse.data;
+    }
+
+    if (serviceResponse && serviceResponse.err) {
+
+        // get boom function
+        let boom = ERRORS[serviceResponse.err.msg];
+
+        if (!boom) {
+            console.error('TODO: unknown error-code:', serviceResponse.err.msg);
+            return boom.badImplementation();
+        }
+
+        // return boom function with (optional) message
+        return boom.fn(serviceResponse.err.details || boom.details);
+    }
+    // TODO: log, really bad - should never happen
+    console.error('really bad:', serviceResponse);
+    return boom.badImplementation();
+
+}

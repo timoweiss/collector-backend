4b067ecaeb0dfaa24c4fd1c9d75a41da3675fe96
Timo Weiß <info@timo-weiss.com>
2016-05-11 11:10:59 +0200

    init metrics

---
+++ api/plugins/metrics/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+exports.register = (server, options, next) => {
+
+    server.route({
+        method: 'GET',
+        path: '/metrics',
+        handler: function (request, reply) {
+            reply('user index');
+        },
+        config: {
+            auth: false
+        }
+    });
+
+
+    server.route({
+        method: 'POST',
+        path: '/metrics',
+        handler: function (request, reply) {
+            reply(request.payload);
+        },
+        config: {
+            auth: false
+        }
+    });
+    const cls = require('continuation-local-storage');
+
+    next();
+};
+
+exports.register.attributes = {
+    name: 'metrics',
+    version: '1.0.0'
+};

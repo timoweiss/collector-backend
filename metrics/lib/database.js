2e2806cb8d9a9deed38bdae15e01b14b51f246f8
Timo Weiß <info@timo-weiss.com>
2016-05-12 23:21:36 +0200

    init database

--- metrics/lib/database.js
+++ metrics/lib/database.js
@@ -1,2 +1,28 @@
 'use strict';
 
+const influxdb = require('influx');
+
+const influxClient = influxdb({
+
+    // or single-host configuration
+    host: 'localhost',
+    port: 8086, // optional, default 8086
+    protocol: 'http', // optional, default 'http'
+    username: 'dbuser',
+    password: 'f4ncyp4ass',
+    database: 'mytestbase'
+});
+
+module.exports = {
+    insertPoints
+};
+
+function insertPoints(seriesName, loadData) {
+    return new Promise((resolve, reject) => {
+        console.time('insert points');
+        influxClient.writePoints(seriesName, loadData, {precision : 'u'}, function (err, resp) {
+            console.log(err, resp);
+            console.timeEnd('insert points');
+        })
+    })
+}

e88a9838d465de578dc2d51129263bf67c9bac2f
Timo Weiß <info@timo-weiss.com>
2016-05-12 15:38:08 +0200

    add POST systems

--- api/plugins/systems/index.js
+++ api/plugins/systems/index.js
@@ -2,6 +2,36 @@
 
 exports.register = (server, options, next) => {
 
+    server.route({
+        method: 'POST',
+        path: '/systems',
+        config: {
+            handler: function (request, reply) {
+
+                const seneca = request.server.seneca;
+                const pattern = request.applyToDefaults({role: 'systems', cmd: 'create'}, request.requesting_user_id);
+
+                seneca.act(pattern, request.payload, function (err, data) {
+
+                    if (err) {
+                        request.logger.error(err, 'create system');
+                        return reply(request.unwrap({err: {msg: 'BAD_IMPL'}}));
+                    }
+
+                    let system = request.unwrap(data);
+
+                    reply(system);
+                });
+            },
+            validate: {
+                payload: validation.register
+            },
+            description: 'create new system',
+            tags: ['api', 'system'],
+            auth: false
+        }
+    });
+
     next();
 };
 

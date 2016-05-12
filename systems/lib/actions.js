8760a795721c61815f5cd8fcd4b837c3d07813f1
Timo Weiß <info@timo-weiss.com>
2016-05-12 15:47:57 +0200

    add created_by as ruid

--- systems/lib/actions.js
+++ systems/lib/actions.js
@@ -9,6 +9,8 @@ module.exports = {
 
 function createSystem(args, callback) {
 
+    args.created_by = args.ruid;
+
     db.createSystem(args)
         .then(response => callback(null, {data: response}))
         .catch(err => callback(err));

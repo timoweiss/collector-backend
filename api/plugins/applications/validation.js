85f1d310da9086d1d6606359516700b393d6c869
Timo Weiß <info@timo-weiss.com>
2016-05-12 16:24:23 +0200

    add createApplication

--- api/plugins/applications/validation.js
+++ api/plugins/applications/validation.js
@@ -8,4 +8,10 @@ validations.id = joi.object().keys({
     id: joi.string().min(5).required()
 });
 
+
+validations.createApplication = joi.object().keys({
+    name: joi.string().min(5).required(),
+    description: joi.string()
+});
+
 module.exports = validations;

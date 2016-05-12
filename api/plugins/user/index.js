bc771022340442d6b1652917bf2781cc74620936
Timo Weiß <info@timo-weiss.com>
2016-05-12 13:42:48 +0200

    implement user register handler, add validation

--- api/plugins/user/index.js
+++ api/plugins/user/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const validation = require('./validation');
+
 exports.register = (server, options, next) => {
 
     server.route({
@@ -14,10 +16,36 @@ exports.register = (server, options, next) => {
     server.route({
         method: 'POST',
         path: '/users',
-        handler: function (request, reply) {
-            reply('user index');
-        },
+
         config: {
+            handler: function (request, reply) {
+                const seneca = request.server.seneca;
+                seneca.act('role:user,cmd:create', request.payload, function (err, data) {
+                    if (err) {
+                        request.logger.error(err, 'register user');
+                        return reply(request.unwrap({err: {msg: 'BAD_IMPL'}}));
+                    }
+
+                    let user = request.unwrap(data);
+
+                    if (user.isBoom) {
+                        return reply(user);
+                    }
+
+                    const sessionData = getSessionData(user);
+
+                    getCompaniesByUserId(user, seneca, resp => {
+
+                        request.cookieAuth.set(sessionData);
+                        reply(resp);
+                    });
+                });
+            },
+            validate: {
+                payload: validation.register
+            },
+            description: 'register new user',
+            tags: ['api', 'user'],
             auth: false
         }
     });

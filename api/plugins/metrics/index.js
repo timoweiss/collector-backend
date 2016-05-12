b24ef32a3ffd4c3b2f1a2575d5d7f8ac496e8712
Timo Weiß <info@timo-weiss.com>
2016-05-12 13:48:26 +0200

    add timeseries test

--- api/plugins/metrics/index.js
+++ api/plugins/metrics/index.js
@@ -1,12 +1,35 @@
 'use strict';
 
+const influxdb = require('influx');
+
+const influxClient = influxdb({
+
+    // or single-host configuration
+    host: 'localhost',
+    port: 8086, // optional, default 8086
+    protocol: 'http', // optional, default 'http'
+    username: 'dbuser',
+    password: 'f4ncyp4ass',
+    database: 'mytestbase'
+})
+
 exports.register = (server, options, next) => {
 
     server.route({
         method: 'GET',
-        path: '/metrics',
+        path: '/metrics/{time?}',
         handler: function (request, reply) {
-            reply('user index');
+            const timeAg = request.params.time ? encodeURIComponent(request.params.time) : '120s';
+            var query = `SELECT mean(value) FROM mytestbase..loadavg WHERE time > 1462997852030000000 GROUP BY time(${timeAg})`;
+
+
+            console.time('query');
+            influxClient.queryRaw(query, function(err, results) {
+                console.timeEnd('query');
+                reply(err || results);
+
+            })
+
         },
         config: {
             auth: false
@@ -18,7 +41,17 @@ exports.register = (server, options, next) => {
         method: 'POST',
         path: '/metrics',
         handler: function (request, reply) {
-            reply(request.payload);
+            // console.log(request.payload.osdata.loadavg)
+            request.payload.osdata.loadavg.forEach(obj => {
+                console.log(obj)
+                influxClient.writePoint('loadavg', obj, null, function(err, resp) {
+                    console.log(err, resp);
+                })
+
+            });
+
+
+            reply(request.payload.osdata.loadavg);
         },
         config: {
             auth: false

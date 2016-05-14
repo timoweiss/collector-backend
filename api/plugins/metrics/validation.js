ecbbdeab11d4a87ffead1c7006555429107f6444
Timo Weiß <info@timo-weiss.com>
2016-05-14 12:44:52 +0200

    add id, update aggregate_fn

--- api/plugins/metrics/validation.js
+++ api/plugins/metrics/validation.js
@@ -5,10 +5,15 @@ const joi = require('joi');
 let validations = {};
 
 validations.loadQuery = joi.object().keys({
-    aggregate_fn: joi.string().valid(['mean', 'median', 'stddev', 'spread', 'min', 'max']).when('period', {is: '7days', then: joi.required()}),
+    aggregate_fn: joi.string().valid(['mean', 'median', 'stddev', 'spread', 'min', 'max']),//.when('period', {is: '7days', then: joi.required()}),
     group_by_value: joi.number().min(1),
     group_by_unit: joi.string().valid(['s', 'm', 'h', 'd']),
     period: joi.string().valid(['5mins', '15mins', '30mins', '60mins', '3hours', '6hours', '12hours', '2days', '7days']).required()
+});
+
+
+validations.id = joi.object().keys({
+    id: joi.string().min(5).required()
 });
 
 

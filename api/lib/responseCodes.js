c327587b974f751787d4eff7effaa2ac00a1cd25
Timo Weiß <info@timo-weiss.com>
2016-05-14 13:34:26 +0200

    add BAD_QUERY

--- api/lib/responseCodes.js
+++ api/lib/responseCodes.js
@@ -14,7 +14,8 @@ const ERRORS = {
     WRONG_PASSWORD: {fn: boom.unauthorized, details: 'Wrong mail or password'},
     BAD_IMPL: {fn: boom.badImplementation},
     MISSING_SYSTEM_ID_SESSION: {fn: boom.badRequest, details: 'Please define a system_id for your session'},
-    USER_ALREADY_EXISTS: {fn: boom.badRequest, details: 'User already exists'}
+    USER_ALREADY_EXISTS: {fn: boom.badRequest, details: 'User already exists'},
+    BAD_QUERY: {fn: boom.badRequest, details: 'This query is crap'}
 };
 
 

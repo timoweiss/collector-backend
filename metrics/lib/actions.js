bc4708510cc6cb22f56da276b04225c2301b25f5
Timo Weiß <info@timo-weiss.com>
2016-05-14 12:59:10 +0200

    import/export query

--- metrics/lib/actions.js
+++ metrics/lib/actions.js
@@ -2,11 +2,13 @@
 
 
 const database = require('./database');
+const query = require('./query');
 
 module.exports = {
     insertLoadavg,
     insertMemory,
-    insertAll
+    insertAll,
+    rawQuery: query.rawQuery
 };
 
 function insertAll(args, callback) {

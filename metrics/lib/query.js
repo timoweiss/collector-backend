32d2ecd614b2b9a8905953352452a52ea87e69d1
Timo Weiß <info@timo-weiss.com>
2016-05-14 12:58:26 +0200

    add query

---
+++ metrics/lib/query.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+const database = require('./database');
+
+module.exports = {
+    rawQuery
+};
+
+function rawQuery(args, callback) {
+    const query = args.raw_query;
+    database.rawQuery(query)
+        .then(result => callback(null, {data: result}))
+        .catch(callback);
+
+}

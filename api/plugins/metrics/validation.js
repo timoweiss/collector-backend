654cd6e8e3c3782c5f0c93c8167bef7be29a432f
Timo Weiß <info@timo-weiss.com>
2016-05-14 13:18:25 +0200

    update time units

--- api/plugins/metrics/validation.js
+++ api/plugins/metrics/validation.js
@@ -8,7 +8,7 @@ validations.loadQuery = joi.object().keys({
     aggregate_fn: joi.string().valid(['mean', 'median', 'stddev', 'spread', 'min', 'max']),//.when('period', {is: '7days', then: joi.required()}),
     group_by_value: joi.number().min(1),
     group_by_unit: joi.string().valid(['s', 'm', 'h', 'd']),
-    period: joi.string().valid(['5mins', '15mins', '30mins', '60mins', '3hours', '6hours', '12hours', '2days', '7days']).required()
+    period: joi.string().valid(['5m', '15m', '30m', '1h', '3h', '6h', '12h', '2d', '7d']).required()
 });
 
 

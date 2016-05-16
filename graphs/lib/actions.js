f8fada29d212117bbafe319a24bd7de717eb32f5
Timo Weiß <info@timo-weiss.com>
2016-05-16 19:22:09 +0200

    add createEvent

--- graphs/lib/actions.js
+++ graphs/lib/actions.js
@@ -4,7 +4,8 @@ const db = require('./database');
 
 module.exports = {
     createSystem,
-    createService
+    createService,
+    createEvent
 };
 
 
@@ -46,4 +47,32 @@ function createService(args, callback) {
             console.log(err);
             callback(err)
         })
+}
+
+function createEvent(args, callback) {
+    const eventNodesData = [];
+    args.requests.forEach(request => {
+        request.annotations.forEach(event => {
+            let eventObj = {
+                type: event.value.toUpperCase(),
+                values: {
+                    requestId: request.request_id,
+                    traceId: request.traceId,
+                    appId: args.app_id,
+                    name: request.name,
+                    timestamp: new Date(event.timestamp / 1000),
+                    duration: request.duration
+                }
+            };
+
+            eventNodesData.push(eventObj);
+        });
+    });
+    let promises = eventNodesData.map(event => db.addNode(event));
+    Promise.all(promises)
+        .then(results => {
+            callback(null, {data: {}});
+            console.log('results creating eventNodes', results);
+        })
+        .catch(err => console.error('error creating eventNodes', err));
 }

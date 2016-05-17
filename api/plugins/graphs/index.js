65e78db4e31503cc965bcc6367069ddfc6a76b2d
Timo Weiß <info@timo-weiss.com>
2016-05-17 14:17:40 +0200

    add transfromGraph, wip

--- api/plugins/graphs/index.js
+++ api/plugins/graphs/index.js
@@ -12,9 +12,16 @@ exports.register = (server, options, next) => {
             handler: function (request, reply) {
 
                 request.query.system_id = request.system_id;
+                console.time('gettingGraph');
+                request.server.seneca.act('role:graphs,cmd:get', request.query, function (err, data) {
+                    if(err) {
+                        return reply(err);
+                    }
 
-                request.server.seneca.act('role:graphs,cmd:get', request.query, function(err, data) {
-                    reply(err || data);
+                    let rawGraphData = request.unwrap(data);
+                    let transformedGraph = transformGraph(rawGraphData.records);
+                    console.timeEnd('gettingGraph');
+                    reply(transformedGraph);
                 });
             },
             description: 'get the network graph for the currently selected system',
@@ -34,3 +41,14 @@ exports.register.attributes = {
     name: 'graphs',
     version: '1.0.0'
 };
+
+function transformGraph(rawData) {
+    let nodes = {};
+    let edges = {};
+    rawData.forEach(elem => {
+        nodes[elem._fields[0].properties.id] = elem._fields[0].properties;
+        nodes[elem._fields[2].properties.id] = elem._fields[2].properties;
+        edges[elem._fields[0].properties.id + '|' + elem._fields[2].properties.id] = elem._fields[1]
+    });
+    return {nodes, edges}
+}

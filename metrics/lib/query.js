986339242e8df89467202d5344a52a010f72e55f
Timo Weiß <info@timo-weiss.com>
2016-05-17 20:00:12 +0200

    init getServiceStats

--- metrics/lib/query.js
+++ metrics/lib/query.js
@@ -14,3 +14,25 @@ function rawQuery(args, callback) {
         .catch(callback);
 
 }
+
+function getServiceStats(args, callback) {
+    let fromTime = args.from;
+    let services = args.services;
+    let queries = [];
+
+    if (typeof services === 'string') {
+        services = [services];
+    }
+
+    services.map(serviceId => {
+        return `SELECT "duration" FROM mytestbase..requests WHERE time > now() - 120m GROUP BY app_id`
+    })
+
+    database.rawQuery()
+        .then(result => console.log({data: result}))
+        .catch(err => console.log('err getServiceStats', err));
+
+    console.log('quering stats for services', services, 'beginning at', fromTime);
+
+    callback(null, {data: {}});
+}

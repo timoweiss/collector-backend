90176ff75dc101b7c09b53d6b7656dd11ced9400
Timo Weiß <info@timo-weiss.com>
2016-05-18 19:27:19 +0200

    format

--- metrics/lib/query.js
+++ metrics/lib/query.js
@@ -27,19 +27,21 @@ function getServiceStats(args, callback) {
     let requestSR = database.rawQuery(`SELECT COUNT("duration") FROM ${DATABASENAME}..requests WHERE time > now() - 3h AND system_id = '${system_id}' AND type = 'SR' GROUP BY app_id`);
     let memory = database.rawQuery(`SELECT MEAN("heapUsed") FROM ${DATABASENAME}..memory WHERE time > now() - 3h AND system_id = '${system_id}' GROUP BY app_id`);
     let loadavg = database.rawQuery(`SELECT MEAN("value") FROM ${DATABASENAME}..loadavg WHERE time > now() - 3h AND system_id = '${system_id}' GROUP BY app_id`);
-        Promise.all([requestCs, requestSR, memory, loadavg])
+    Promise.all([requestCs, requestSR, memory, loadavg])
         .then(result => {
-            if(!result[0][0] || !result[0][0].series) {
+            if (!result[0][0] || !result[0][0].series) {
                 console.error('why', result[0]);
             }
-            callback(null, {data: {
-                requests: {
-                    CS: result[0][0].series || [],
-                    SR: result[1][0].series || []
-                },
-                memory: result[2][0].series,
-                loadavg: result[3][0].series
-            }})
+            callback(null, {
+                data: {
+                    requests: {
+                        CS: result[0][0].series || [],
+                        SR: result[1][0].series || []
+                    },
+                    memory: result[2][0].series,
+                    loadavg: result[3][0].series
+                }
+            })
         })
         .catch(err => {
             callback(err);

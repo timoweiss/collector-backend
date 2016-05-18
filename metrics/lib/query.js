23b10d4b9aef6aa341aed10d4f260d8543fbc902
Timo Weiß <info@timo-weiss.com>
2016-05-18 19:26:27 +0200

    read influxdb dbname from env

--- metrics/lib/query.js
+++ metrics/lib/query.js
@@ -3,6 +3,8 @@
 
 const database = require('./database');
 
+const DATABASENAME = process.env['INFLUXDB_DATABASENAME'] || 'mytestbase';
+
 module.exports = {
     rawQuery,
     getServiceStats
@@ -20,11 +22,11 @@ function getServiceStats(args, callback) {
     let fromTime = args.from;
     let system_id = args.system_id;
 
-    // TODO: hardcoded time, database
-    let requestCs = database.rawQuery(`SELECT COUNT("duration") FROM mytestbase..requests WHERE time > now() - 3h AND system_id = '${system_id}' AND type = 'CS' GROUP BY app_id`);
-    let requestSR = database.rawQuery(`SELECT COUNT("duration") FROM mytestbase..requests WHERE time > now() - 3h AND system_id = '${system_id}' AND type = 'SR' GROUP BY app_id`);
-    let memory = database.rawQuery(`SELECT MEAN("heapUsed") FROM mytestbase..memory WHERE time > now() - 3h AND system_id = '${system_id}' GROUP BY app_id`);
-    let loadavg = database.rawQuery(`SELECT MEAN("value") FROM mytestbase..loadavg WHERE time > now() - 3h AND system_id = '${system_id}' GROUP BY app_id`);
+    // TODO: hardcoded time
+    let requestCs = database.rawQuery(`SELECT COUNT("duration") FROM ${DATABASENAME}..requests WHERE time > now() - 3h AND system_id = '${system_id}' AND type = 'CS' GROUP BY app_id`);
+    let requestSR = database.rawQuery(`SELECT COUNT("duration") FROM ${DATABASENAME}..requests WHERE time > now() - 3h AND system_id = '${system_id}' AND type = 'SR' GROUP BY app_id`);
+    let memory = database.rawQuery(`SELECT MEAN("heapUsed") FROM ${DATABASENAME}..memory WHERE time > now() - 3h AND system_id = '${system_id}' GROUP BY app_id`);
+    let loadavg = database.rawQuery(`SELECT MEAN("value") FROM ${DATABASENAME}..loadavg WHERE time > now() - 3h AND system_id = '${system_id}' GROUP BY app_id`);
         Promise.all([requestCs, requestSR, memory, loadavg])
         .then(result => {
             if(!result[0][0] || !result[0][0].series) {

9f3011fa6d94b57235cdaeba696596ba3e08c2a7
Timo Weiß <info@timo-weiss.com>
2016-05-18 17:10:57 +0200

    add debug log

--- metrics/lib/query.js
+++ metrics/lib/query.js
@@ -26,14 +26,19 @@ function getServiceStats(args, callback) {
     let memory = database.rawQuery(`SELECT MEAN("heapUsed") FROM mytestbase..memory WHERE time > now() - 3h AND system_id = '${system_id}' GROUP BY app_id`)
     let loadavg = database.rawQuery(`SELECT MEAN("value") FROM mytestbase..loadavg WHERE time > now() - 3h AND system_id = '${system_id}' GROUP BY app_id`)
         Promise.all([requestCs, requestSR, memory, loadavg])
-        .then(result => callback(null, {data: {
-            requests: {
-                CS: result[0][0].series,
-                SR: result[1][0].series
-            },
-            memory: result[2][0].series,
-            loadavg: result[3][0].series
-        }}))
+        .then(result => {
+            if(!result[0][0] || !result[0][0].series) {
+                console.error('why', result[0]);
+            }
+            callback(null, {data: {
+                requests: {
+                    CS: result[0][0].series || [],
+                    SR: result[1][0].series || []
+                },
+                memory: result[2][0].series,
+                loadavg: result[3][0].series
+            }})
+        })
         .catch(err => {
             callback(err);
             console.log('err getServiceStats', err)

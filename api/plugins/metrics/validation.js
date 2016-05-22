45d91cf959e06ff2dcb9f3c4e7a10270461994d5
Timo Weiß <info@timo-weiss.com>
2016-05-22 16:39:39 +0200

    extend validation, dup to loadavg

--- api/plugins/metrics/validation.js
+++ api/plugins/metrics/validation.js
@@ -12,6 +12,9 @@ validations.loadQuery = joi.object().keys({
 });
 
 validations.memoryQuery = joi.object().keys({
+    aggregate_fn: joi.string().valid(['mean', 'median', 'stddev', 'spread', 'min', 'max']),//.when('period', {is: '7days', then: joi.required()}),
+    group_by_value: joi.number().min(1),
+    group_by_unit: joi.string().valid(['s', 'm', 'h', 'd']),
     period: joi.string().valid(['5m', '15m', '30m', '1h', '3h', '6h', '12h', '2d', '7d']).required()
 });
 

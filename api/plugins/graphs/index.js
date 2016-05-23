32489914ce5399a7bf6eaa2b09d5207936567666
Timo Weiß <info@timo-weiss.com>
2016-05-23 14:14:05 +0200

    add /graphs/traces/{id}

--- api/plugins/graphs/index.js
+++ api/plugins/graphs/index.js
@@ -40,6 +40,46 @@ exports.register = (server, options, next) => {
     });
 
 
+    server.route({
+        method: 'GET',
+        path: '/graphs/traces/{id}',
+        config: {
+            handler: function (request, reply) {
+
+                if (!request.system_id) {
+                    return reply(request.unwrap({err: {msg: 'MISSING_SYSTEM_ID_SESSION'}}))
+                }
+
+                let query = {
+                    system_id: request.system_id,
+                    traceId: request.params.id
+                };
+                console.log();
+
+                console.time('gettingGraph for traceid');
+                request.server.seneca.act('role:graphs,cmd:get', query, function (err, data) {
+                    if (err) {
+                        console.error(err);
+                        return reply(err);
+                    }
+
+                    let graphData = request.unwrap(data);
+
+                    reply(graphData);
+                    console.timeEnd('gettingGraph for traceid');
+                });
+
+
+            },
+            description: 'get the network graph for selected trace id',
+            tags: ['api', 'system', 'graph', 'trace'],
+            validate: {
+                params: validation.id
+            }
+        }
+    });
+
+
     next();
 
 };

635cbe4c7b0b1d9e88ebadeb8fee0ba4bae8a731
Timo Weiß <info@timo-weiss.com>
2016-05-24 16:34:17 +0200

    add timeQuery

--- api/plugins/metrics/validation.js
+++ api/plugins/metrics/validation.js
@@ -18,6 +18,12 @@ validations.memoryQuery = joi.object().keys({
     period: joi.string().valid(['5m', '15m', '30m', '1h', '3h', '6h', '12h', '2d', '7d']).required()
 });
 
+validations.timeQuery = joi.object().keys({
+    period: joi.string().valid(['5m', '10m', '15m', '30m', '1h', '2h', '3h', '4h', '12h', '1d', '2d', '3d', '7d']),
+    from: joi.date(),
+    to: joi.date()
+});
+
 
 validations.id = joi.object().keys({
     id: joi.string().min(5).required()

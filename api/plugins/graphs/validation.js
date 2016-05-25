cf8572220544c6fb992102b06e0d59b215d98f96
Timo Weiß <info@timo-weiss.com>
2016-05-25 17:17:54 +0200

    add timeQuery

--- api/plugins/graphs/validation.js
+++ api/plugins/graphs/validation.js
@@ -13,4 +13,10 @@ validations.id = joi.object().keys({
     id: joi.string().required()
 });
 
+validations.timeQuery = joi.object().keys({
+    since: joi.string().valid(['5m', '10m', '15m', '30m', '1h', '2h', '4h', '12h', '1d', '2d', '3d', '7d']),
+    from: joi.date(),
+    to: joi.date()
+});
+
 module.exports = validations;

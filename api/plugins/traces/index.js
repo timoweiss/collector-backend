19caacb2c861b5f9ddd1a3077faee4413d165713
Timo Weiß <info@timo-weiss.com>
2016-06-16 14:40:34 +0200

    init traces

--- api/plugins/traces/index.js
+++ api/plugins/traces/index.js
@@ -1,6 +1,85 @@
 'use strict';
 
+const validation = require('./validation');
+
 exports.register = (server, options, next) => {
+
+    server.route({
+        method: 'GET',
+        path: '/traces',
+        config: {
+            handler: function (request, reply) {
+
+                if (!request.system_id) {
+                    return reply(request.unwrap({err: {msg: 'MISSING_SYSTEM_ID_SESSION'}}))
+                }
+
+                let query = {
+                    system_id: request.system_id
+                };
+
+                console.time('getting traces');
+                request.server.seneca.act('role:graphs,cmd:get,type:traces', query, function (err, data) {
+                    if (err) {
+                        console.error(err);
+                        return reply(err);
+                    }
+
+                    let graphData = request.unwrap(data);
+
+                    reply(graphData);
+                    console.timeEnd('getting traces');
+                });
+
+
+            },
+            description: 'get the recent traces for current selected system',
+            tags: ['api', 'system', 'trace']
+        }
+    });
+
+
+
+    server.route({
+        method: 'GET',
+        path: '/traces/{id}',
+        config: {
+            handler: function (request, reply) {
+
+                if (!request.system_id) {
+                    return reply(request.unwrap({err: {msg: 'MISSING_SYSTEM_ID_SESSION'}}))
+                }
+
+                let query = {
+                    system_id: request.system_id,
+                    traceId: request.params.id
+                };
+                console.log();
+
+                console.time('gettingGraph for traceid');
+                request.server.seneca.act('role:graphs,cmd:get', query, function (err, data) {
+                    if (err) {
+                        console.error(err);
+                        return reply(err);
+                    }
+
+                    let graphData = request.unwrap(data);
+
+                    reply(graphData);
+                    console.timeEnd('gettingGraph for traceid');
+                });
+
+
+            },
+            description: 'get the network graph for selected trace id',
+            tags: ['api', 'system', 'graph', 'trace'],
+            validate: {
+                params: validation.id
+            }
+        }
+    });
+
+
 
     next();
 };

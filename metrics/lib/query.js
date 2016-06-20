dbb5128f6dfa601d8f0fe5fb0900574aee5150b9
Timo Weiß <info@timo-weiss.com>
2016-06-20 15:05:33 +0200

    update and extend cqs, update database name

--- metrics/lib/query.js
+++ metrics/lib/query.js
@@ -3,7 +3,7 @@
 
 const database = require('./database');
 
-const DATABASENAME = process.env['INFLUXDB_DATABASENAME'] || 'mytestbase';
+const DATABASENAME = process.env['INFLUXDB_DATABASENAME'] || 'development';
 
 // default for the oldest data to retreive
 const OLDEST_METRIC_DATA = process.env['OLDEST_METRIC_DATA'] || '7d';
@@ -36,25 +36,49 @@ const DOWNSAMPLED_CQS = [{
     from: 'default',
     source_name: 'loadavg',
     into: '6sec_bucket',
-    downsampled_name: 'downsampled_loadavg',
+    downsampled_name: 'loadavg',
     interval: '6s',
     select_stmt: 'SELECT mean(value) as value_mean, median(value) as value_median, percentile(value, 95) as value_percentile_95, percentile(value, 99) as value_percentile_99'
 }, {
     name: 'cq_144s_loadavg',
     from: '6sec_bucket',
-    source_name: 'downsampled_loadavg',
+    source_name: 'loadavg',
     into: '144sec_bucket',
-    downsampled_name: 'downsampled_loadavg',
+    downsampled_name: 'loadavg',
     interval: '144s',
     select_stmt: 'SELECT mean(value_mean) as value_mean, median(value_median) as value_median, percentile(value_percentile_95, 95) as value_percentile_95, percentile(value_percentile_99, 99) as value_percentile_99'
 }, {
     name: 'cq_30m_loadavg',
     from: '144sec_bucket',
-    source_name: 'downsampled_loadavg',
+    source_name: 'loadavg',
     into: '30min_bucket',
-    downsampled_name: 'downsampled_loadavg',
+    downsampled_name: 'loadavg',
     interval: '30m',
     select_stmt: 'SELECT mean(value_mean) as value_mean, median(value_median) as value_median, percentile(value_percentile_95, 95) as value_percentile_95, percentile(value_percentile_99, 99) as value_percentile_99'
+}, {
+    name: 'cq_6s_requests',
+    from: 'default',
+    source_name: 'requests',
+    into: '6sec_bucket',
+    downsampled_name: 'requests',
+    interval: '6s',
+    select_stmt: 'SELECT mean("duration") as duration_mean, median("duration") as duration_median, percentile("duration", 95) as duration_percentile_95, percentile("duration", 99) as duration_percentile_99'
+}, {
+    name: 'cq_144s_requests',
+    from: '6sec_bucket',
+    source_name: 'requests',
+    into: '144sec_bucket',
+    downsampled_name: 'requests',
+    interval: '144s',
+    select_stmt: 'SELECT mean(duration_mean) as duration_mean, median(duration_median) as duration_median, percentile(duration_percentile_95, 95) as duration_percentile_95, percentile(duration_percentile_99, 99) as duration_percentile_99'
+}, {
+    name: 'cq_30m_requests',
+    from: '144sec_bucket',
+    source_name: 'requests',
+    into: '30min_bucket',
+    downsampled_name: 'requests',
+    interval: '30m',
+    select_stmt: 'SELECT mean(duration_mean) as duration_mean, median(duration_median) as duration_median, percentile(duration_percentile_95, 95) as duration_percentile_95, percentile(duration_percentile_99, 99) as duration_percentile_99'
 }];
 
 const RP_BUCKETS = [{

e6f65ce7e1b268a603724832e6647ed99a82f32c
Timo Weiß <info@timo-weiss.com>
2016-06-20 15:04:59 +0200

    include tags in continuous queries

--- metrics/lib/database.js
+++ metrics/lib/database.js
@@ -25,7 +25,7 @@ function createCQFromBuckets(cqs, DATABASENAME) {
 
     const all = cqs.map(cq => {
         return new Promise((resolve, reject) => {
-            const stmt = `${cq.select_stmt} INTO ${DATABASENAME}."${cq.into}".${cq.downsampled_name} FROM ${DATABASENAME}."${cq.from}".${cq.source_name} GROUP BY time(${cq.interval}) `;
+            const stmt = `${cq.select_stmt} INTO ${DATABASENAME}."${cq.into}".${cq.downsampled_name} FROM ${DATABASENAME}."${cq.from}".${cq.source_name} GROUP BY time(${cq.interval}), * `;
             // console.log(stmt);
             influxClient.createContinuousQuery(cq.name, stmt, (err, res) => {
                 if(err) {
